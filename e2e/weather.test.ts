3d459621790199ade098d7e72a1a831ceb9b2daa
Fábio Henriques <fabio.henriques@callstack.com>
2023-01-26 18:23:04 +0000

    Implement e2e test validation for show notification button

--- e2e/weather.test.ts
+++ e2e/weather.test.ts
@@ -2,14 +2,17 @@ import 'detox';
 
 describe('Weather city list and details', () => {
   beforeAll(async () => {
-    await device.launchApp({newInstance: true});
+    await device.launchApp({
+      newInstance: true,
+      permissions: {notifications: 'YES'},
+    });
   });
 
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
-  it('should show the weather city list screen, tap the "Paris" item and navigate to its details screen', async () => {
+  it('should show the weather city list screen, tap the "Paris" item and navigate to its details screen and tap the button', async () => {
     await expect(element(by.id('weather-city-list-screen'))).toBeVisible();
 
     await expect(element(by.text('Paris'))).toBeVisible();
@@ -19,5 +22,7 @@ describe('Weather city list and details', () => {
     await expect(element(by.id('weather-city-details-screen'))).toBeVisible();
 
     await expect(element(by.text('Paris')).atIndex(0)).toBeVisible();
+
+    await element(by.text('Show notification')).tap();
   });
 });
