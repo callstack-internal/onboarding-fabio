914dea614d96d7f77cf907550c4415b291de76fe
Fábio Henriques <fabio.henriques@callstack.com>
2023-01-26 18:23:35 +0000

    Refactor tests to remove normal snapshots e leave only the diff ones

--- src/features/weather/city-details/WeatherCityDetailsInfoItem.test.tsx
+++ src/features/weather/city-details/WeatherCityDetailsInfoItem.test.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import WeatherCityDetailsInfoItem from './WeatherCityDetailsInfoItem';
-
-test('should render correctly', () => {
-  const tree = renderer
-    .create(<WeatherCityDetailsInfoItem label={'Cloud Cover'} value={'50%'} />)
-    .toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
 
 test('should render a different state correctly', () => {
   expect(
